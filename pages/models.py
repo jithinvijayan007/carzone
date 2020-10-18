3b0d0ba376346503644d8737a14d3b3ea8b672ad
Jithin <67956333+jithinvijayan007@users.noreply.github.com>
2020-10-18 23:29:06 +0530

    Models updatted

--- pages/models.py
+++ pages/models.py
@@ -3,6 +3,37 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
+
+class Car(models.Model):
+    Service_Choices=[('Platinum','Platinum'),('Gold','Gold')]
+    car_model=models.CharField(max_length=100)
+    car_owner=models.CharField(max_length=100)
+    car_notes=models.CharField(max_length=100)
+    car_number=models.CharField(max_length=100)
+    description=models.TextField()
+    service_type=models.CharField(max_length=10,choices=Service_Choices,blank=True)
+    submission_date=models.DateField()
+    year_old=models.CharField(max_length=20,null=True)
+    servicing=models.ManyToManyField('Service',blank=True)
+
+    def __str__(self):
+        return self.car_model +"     "+self.car_owner
+
+class Service(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class ProfileModel(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    Mob_Number=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
+
+    
+
 class CustomUserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""
 
